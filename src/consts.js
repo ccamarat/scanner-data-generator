566e6fb1de2bc393aa18111ebee08cdd59bda053
Chris <chris.camaratta@hotmail.com>
2019-02-04 17:15:02 -0800

    converted to console app

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,8 @@
 export const speed = 100;
 export const updateInterval = 10;
 export const scannerCount = 4;
-export const runtime = 9999;
+export const runtime = 1500;
+
+export const databaseConfig = {
+
+};
